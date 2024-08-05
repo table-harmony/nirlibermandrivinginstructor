8c1b44a3b29af3189677643c2c29b68eb309afd2
table-harmony <lironkaner2007@gmail.com>
2024-08-05 15:50:46 +0300

    adding components

--- src/site-config.ts
+++
@@ -1,7 +0,0 @@
-export const siteConfig = {
-  name: "Nir Liberman driving instructor",
-  url: "https://nirlibermandrivinginstructor.vercel.app",
-  links: {},
-};
-
-export type SiteConfig = typeof siteConfig;
