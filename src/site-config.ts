ee371028a8b938055a459497e06ac3305de01483
table-harmony <lironkaner2007@gmail.com>
2024-08-05 13:51:56 +0300

    adding react

---
+++ src/site-config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "Nir Liberman driving instructor",
+  url: "https://nirlibermandrivinginstructor.vercel.app",
+  links: {},
+};
+
+export type SiteConfig = typeof siteConfig;
