b2573079f9d6372cb02b5d7ea386e9f0879d8abe
table-harmony <lironkaner2007@gmail.com>
2024-08-09 17:36:35 +0300

    Rewriting

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: 'NirLibermanDrivingInstructor',
+  description: 'Nir Liberman driving instructor',
+  url: 'https://nirlibermandrivinginstructor.vercel.app',
+  author: 'table-harmony',
+  links: {},
+}
+
+export type SiteConfig = typeof siteConfig
