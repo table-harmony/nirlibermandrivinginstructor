6d7060ff2cb18d245b54b6e4b41af14fa8b7bb77
table-harmony <lironkaner2007@gmail.com>
2024-08-10 20:45:45 +0300

    footer and header

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { Switch } from './ui/switch'
+import { cn } from '@/lib/utils'
+import { MoonIcon, SunIcon } from 'lucide-react'
 
 export function ModeToggle() {
   const [theme, setTheme] = React.useState(() => {
@@ -32,12 +33,23 @@ export function ModeToggle() {
 
   return (
     <div dir="ltr">
-      <span className="sr-only">{theme === 'dark' ? 'dark' : 'light'}</span>
-      <Switch
-        aria-label="theme switcher"
-        checked={theme === 'dark'}
-        onClick={toggleTheme}
-      />
+      <div className="flex items-center space-x-2 rounded-full border p-1">
+        <button
+          onClick={toggleTheme}
+          className={cn('rounded-full p-1.5', theme === 'light' && 'bg-muted')}
+        >
+          <span className="sr-only">light</span>
+
+          <SunIcon className="h-4 w-4" />
+        </button>
+        <button
+          onClick={toggleTheme}
+          className={cn('rounded-full p-1.5', theme === 'dark' && 'bg-muted')}
+        >
+          <span className="sr-only">dark</span>
+          <MoonIcon className="h-4 w-4" />
+        </button>
+      </div>
     </div>
   )
 }
